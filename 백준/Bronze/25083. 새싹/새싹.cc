9d2f466a8127f5bab871d06fbdf1e167a2adf781
Hanyoung Lee <kakaoleaves@naver.com>
2023-06-27 14:52:03 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << R"(         ,r'"7)" << endl;
+	cout << R"(r`-_   ,'  ,/)" << endl;
+	cout << R"( \. ". L_r')" << endl;
+	cout << R"(   `~\/)" << endl;
+	cout << R"(      |)" << endl;
+	cout << R"(      |)" << endl;
+		
+	return 0;
+}
