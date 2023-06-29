07480d40e9392697c617c786764d35b0da414303
Hanyoung Lee <kakaoleaves@naver.com>
2023-06-29 14:51:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1764 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+using namespace std;
+
+int counting[10001];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    assert(n >= 1 && n <= 10000000);
+
+    for (int i = 0; i < n; i++)
+    {
+		int num;
+		cin >> num;
+		assert(num >= 1 && num <= 10000);
+        int index = num - 1;
+		counting[index]++;
+	}
+
+    for (int i = 0; i < 10000; i++)
+    {
+        for (int j = 0; j < counting[i]; j++)
+        {
+			cout << i + 1 << "\n";
+		}
+    }
+
+    return 0;
+}
