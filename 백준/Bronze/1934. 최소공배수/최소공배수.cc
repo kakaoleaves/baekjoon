0a3268b7c51d521e073d198a87d47a8efc8c7dd0
Hanyoung Lee <kakaoleaves@naver.com>
2023-06-30 22:34:29 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int n;
-
 	cin >> n;
-
 	for (int i = 0; i < n; i++)
 	{
 		int a, b;
 		cin >> a >> b;
-		int gcd = 1;
-		for (int j = 1; j <= min(a, b); j++)
-		{
-			if (a % j == 0 && b % j == 0)
-				gcd = j;
-		}
-		int lcm = a * b / gcd;
-		cout << lcm << '\n';
+		cout << lcm(a, b) << '\n';
 	}
 
 	return 0;
