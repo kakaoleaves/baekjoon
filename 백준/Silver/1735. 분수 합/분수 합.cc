f37140b8d6aff10381ec32ee28f5e2bcba571a77
Hanyoung Lee <kakaoleaves@naver.com>
2023-06-30 22:32:28 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.cc
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -8,31 +16,13 @@ int main()
 
 	int a, b, c, d;
 	cin >> a >> b >> c >> d;
-	int gcd = 1;
-	int lcm = 1;
 
-	for (int i = 0; i < min(b, d); i++)
-	{
-		if (b % (i + 1) == 0 && d % (i + 1) == 0)
-		{
-			gcd = i + 1;
-		}
-	}
+	int numerator = a * d + b * c;
+	int denominator = b * d;
 
-	lcm = (b * d) / gcd;
+	int gcd_num = gcd(numerator, denominator);
 
-	int numerator = a * (lcm / b) + c * (lcm / d);
-	int denominator = lcm;
-
-	for (int i = 0; i < min(numerator, denominator); i++)
-	{
-		if (numerator % (i + 1) == 0 && denominator % (i + 1) == 0)
-		{
-			gcd = i + 1;
-		}
-	}
-
-	cout << numerator / gcd << " " << denominator / gcd << "\n";
+	cout << numerator / gcd_num << " " << denominator / gcd_num << "\n";
 
 	return 0;
 }
