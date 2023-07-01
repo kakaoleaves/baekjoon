a60c979c67a03f17e4b4ca13bea6794982c9ac88
Hanyoung Lee <kakaoleaves@naver.com>
2023-07-01 11:12:30 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int i;
+	cin >> i;
+	cout << (int)pow(2, i);
+
+	return 0;
+}
