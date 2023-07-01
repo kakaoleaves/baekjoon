afcd6f734d5cfce3f0a60012cd8d567a4b1bdb08
Hanyoung Lee <kakaoleaves@naver.com>
2023-07-01 17:56:44 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	int m;
+	cin >> n >> m;
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	cout << "<";
+	while (!q.empty())
+	{
+		for (int i = 0; i < m - 1; i++)
+		{
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty())
+			cout << ", ";
+	}
+	cout << ">\n";
+
+	return 0;
+}
