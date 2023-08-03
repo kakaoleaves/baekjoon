1f80ccfe2dc78db53960a3768abc4d441f260f56
Hanyoung Lee <kakaoleaves@naver.com>
2023-08-03 14:12:33 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int cnt = 0;
-int degree = 0;
-
-int MenOfPassion(vector<int>& v, int n)
-{
-    int i = n / 2;
-    cnt++;
-    return v[i];
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n; // 1 <= n <= 500,000
+    int n; // 1 <= n <= 500000
     cin >> n;
 
-    vector<int> v(n, 1);
-
-    MenOfPassion(v, n);
-
-    cout << cnt << endl;
-    cout << degree << endl;
-
+    cout << 1 << '\n' << 0 << '\n';
 
     return 0;
 }
