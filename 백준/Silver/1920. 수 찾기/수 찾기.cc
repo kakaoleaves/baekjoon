82001ea976cb7b4b5d1560c87adc535defa587a6
Hanyoung Lee <kakaoleaves@naver.com>
2023-09-15 14:39:16 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,58 +1,54 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdio>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int arr[100001];
+bool binarySearch(vector<int>& v, int num) {
+    int left = 0;
+	int right = (int)v.size() - 1;
 
-//이진탐색(Binary Search)을 이용하여 탐색
-void Solution(int n, int key) {
+    while (left <= right) {
+		int mid = (left + right) / 2;
 
-    int start = 0;
-    int end = n - 1;
-    int mid;
+        if (v[mid] == num) {
+			return true;
+		}
+        else if (v[mid] < num) {
+			left = mid + 1;
+		}
+        else {
+			right = mid - 1;
+		}
+	}
 
-    while (end - start >= 0) {
-        mid = (start + end) / 2;
-
-        if (arr[mid] == key) {   //key 값이 배열의 중앙 값과 같을때
-            printf("1\n");
-            return;
-
-        }
-        else if (arr[mid] > key) { //key 값이 배열의 중앙 값보다 작을때 (왼쪽으로)
-            end = mid - 1;
-
-        }
-        else {  //key 값이 배열의 중앙 값보다 클때 (오른쪽으로)
-            start = mid + 1;
-        }
-    }
-
-    printf("0\n");
-    return;
+	return false;
 }
 
-int main(void) {
 
-    int n, m, tmp;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    //입력
-    scanf("%d", &n);
+    int n, m;
+    cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    sort(arr, arr + n);   //quick sort를 이용해 배열 오름차순으로 정렬
+    vector<int> v(n);
 
-    //입력
-    scanf("%d", &m);
+    for (auto& i : v)
+		cin >> i;
 
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &tmp);
-        Solution(n, tmp);
+    sort(v.begin(), v.end());
+
+    cin >> m;
+
+    while (m--)
+    {
+        int num;
+        cin >> num;
+        cout << binarySearch(v, num) << '\n';
     }
 
     return 0;
