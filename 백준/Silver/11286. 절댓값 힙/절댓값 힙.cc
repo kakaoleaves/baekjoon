f270c2b3c9902867464d38b15d0ff0b513e5ddd8
Hanyoung Lee <kakaoleaves@naver.com>
2023-09-24 18:14:03 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct Compare {
+    bool operator()(const int& a, const int& b) {
+		if (abs(a) == abs(b)) return a > b;
+		return abs(a) > abs(b);
+	}
+};
+
+int main( )
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    priority_queue<int, vector<int>, Compare> pq;
+
+    int n; // 1 <= n <= 100'000
+    cin >> n;
+
+    while (n--) {
+        int op;
+        cin >> op;
+        if (op != 0)
+            pq.emplace(op);
+        else {
+            if (pq.empty()) cout << 0 << '\n';
+            else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+        }
+    }
+
+    return 0;
+}
