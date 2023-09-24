cc29fdb4d24406b9b72796ca82c11ebf5eab47b0
Hanyoung Lee <kakaoleaves@naver.com>
2023-09-24 18:02:10 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main( )
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    priority_queue<int, vector<int>, less<int>> pq;
+
+    int n; // 1 <= n <= 100'000
+    cin >> n;
+
+    while (n--) {
+        int op;
+        cin >> op;
+        if (op > 0)
+            pq.emplace(op);
+        else {
+            if (pq.empty()) cout << 0 << '\n';
+            else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+        }
+    }
+
+    return 0;
+}
