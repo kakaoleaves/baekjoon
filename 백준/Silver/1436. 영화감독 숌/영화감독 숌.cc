ad7f91b10b2b8aedcc47227cb21e49b7c7f5f0af
Hanyoung Lee <kakaoleaves@naver.com>
2024-03-06 21:56:56 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int n; // 1 < = n <= 10'000
 
-    int n;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     cin >> n;
 
+    int num = 0;
     int cnt = 0;
-    int num = 666;
 
-    while (true) {
-		string str = to_string(num);
-        if (str.find("666") != string::npos) {
+    while (cnt < n) {
+        num++;
+        string s = to_string(num);
+        if (s.find("666") != string::npos) {
 			cnt++;
 		}
-        if (cnt == n) {
-			cout << num << endl;
-			break;
-		}
-		num++;
-	}
+    }
+
+    cout << num << '\n';
 
     return 0;
 }
