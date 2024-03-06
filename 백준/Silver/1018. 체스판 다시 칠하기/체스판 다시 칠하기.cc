939083101750bbc444007ef6c1b1d3ecc834bbaa
Hanyoung Lee <kakaoleaves@naver.com>
2024-03-06 21:42:04 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,46 +1,52 @@
 #include <iostream>
-#include <cassert>
+#include <vector>
+#include <limits>
+#include <algorithm>
 using namespace std;
 
-char board[50][50];
+int n, m; // 8 <= n, m <= 50
+vector<vector<char>> board(50, vector<char>(50, 0));
+vector<vector<char>> whiteBoard(8, vector<char>(8, 'W'));
+vector<vector<char>> blackBoard(8, vector<char>(8, 'B'));
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    int n, m;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            whiteBoard[i][j] = (i + j) % 2 == 0 ? 'W' : 'B';
+
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+			blackBoard[i][j] = (i + j) % 2 == 0 ? 'B' : 'W';
+
     cin >> n >> m;
-    assert(n >= 8 && n <= 50 && m >= 8 && m <= 50);
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-        {
             cin >> board[i][j];
-            assert(board[i][j] == 'W' || board[i][j] == 'B');
-        }
 
-    int min = 64;
+    int ans = numeric_limits<int>::max();
 
     for (int i = 0; i <= n - 8; i++)
-        for (int j = 0; j <= m - 8; j++)
-        {
-            int cnt = 0;
-            for (int k = i; k < i + 8; k++)
-                for (int l = j; l < j + 8; l++)
-                {
-                    if ((k + l) % 2 == 0 && board[k][l] == 'B')
-						cnt++;
-					else if ((k + l) % 2 == 1 && board[k][l] == 'W')
-						cnt++;
-                }
-            if (cnt > 32)
-                cnt = 64 - cnt;
-            if (cnt < min)
-                min = cnt;
+        for (int j = 0; j <= m - 8; j++) {
+            int whiteCount = 0;
+			int blackCount = 0;
+
+			for (int k = 0; k < 8; k++)
+                for (int l = 0; l < 8; l++) {
+					if (board[i + k][j + l] != whiteBoard[k][l])
+						whiteCount++;
+					if (board[i + k][j + l] != blackBoard[k][l])
+						blackCount++;
+				}
+
+            ans = min(ans, min(whiteCount, blackCount));
         }
 
-    cout << min << endl;
+    cout << ans << '\n';
 
     return 0;
 }
