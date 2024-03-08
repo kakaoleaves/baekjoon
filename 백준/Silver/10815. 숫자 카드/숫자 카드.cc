bc6bdb338bfe5960008d8fb98d33a3b3bb8c5fc6
Hanyoung Lee <kakaoleaves@naver.com>
2024-03-08 10:11:22 +0900

    [Silver V] Title: 숫자 카드, Time: 220 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,33 +1,31 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+int n;
+vector<int> v(500'000);
+int m;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n;
-	int m;
-	set<int> s;
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	cin >> n;
 
 	for (int i = 0; i < n; i++)
-	{
-		int num;
-		cin >> num;
-		s.insert(num);
-	}
+		cin >> v[i];
+
+	sort(v.begin(), v.begin() + n);
 
 	cin >> m;
 
-	for (int i = 0; i < m; i++)
-	{
-		int num;
-		cin >> num;
-		cout << s.count(num) << '\n';
+	int x;
+	for (int i = 0; i < m; i++) {
+		cin >> x;
+		cout << binary_search(v.begin(), v.begin() + n, x) << ' ';
 	}
 
 	return 0;
