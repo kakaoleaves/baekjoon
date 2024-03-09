6cbef376bd0c70080940a926fddeb7aefeeaa8cd
Hanyoung Lee <kakaoleaves@naver.com>
2024-03-09 16:28:32 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 100 ms, Memory: 6820 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -1,35 +1,53 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void star(int i, int j, int n)
-{
-	if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
-		cout << ' ';
-	else
-	{
-		if (n / 3 == 0)
-			cout << '*';
-		else
-			star(i, j, n / 3);
+int n; // 3 <= n <= 3^8
+vector<vector<char>> star;
+
+void CantorStar(int x1, int x2, int y1, int y2) {
+	if (x2 - x1 < 3 && y2 - y1 < 3) {
+		star[x1 + 1][y1 + 1] = ' ';
+	}
+	else {
+		int x = (x2 - x1 + 1) / 3;
+		int y = (y2 - y1 + 1) / 3;
+
+		for (int i = x1 + x; i < x1 + 2 * x; i++) {
+			for (int j = y1 + y; j < y1 + 2 * y; j++) {
+				star[i][j] = ' ';
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!(i == 1 && j == 1)) {
+					CantorStar(x1 + i * x, x1 + (i + 1) * x - 1, y1 + j * y, y1 + (j + 1) * y - 1);
+				}
+			}
+		}
+
 	}
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(nullptr);
 
-	int n;
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			star(i, j, n);
+
+	star.resize(n, vector<char>(n, '*'));
+
+	CantorStar(0, n - 1, 0, n - 1);
+
+	for (auto& row : star) {
+		for (auto& e : row) {
+			cout << e;
+		}
 		cout << '\n';
 	}
-
-
 
 	return 0;
 }
