5fc1f66ac26d58c8a0d855ffba009d3148d02ddc
Hanyoung Lee <kakaoleaves@naver.com>
2024-03-11 15:21:56 +0900

    [Gold IV] Title: 스도쿠, Time: 212 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,87 +1,83 @@
 #include <iostream>
 #include <vector>
-#define fastio cin.tie(0)->sync_with_stdio(0)
-
+#include <cmath>
 using namespace std;
 
-#define SUDOKU_SIZE 9
-#define SMALL_SUDOKU_SIZE 3
+vector<vector<short>> sudoku(9, vector<short>(9, 0));
+vector<pair<short, short>> targets;
 
-void print(vector<vector<int>> const& s)
-{
-	for (auto const& i : s)
-	{
-		for (auto const& j : i)
-			cout << j << " ";
-		cout << "\n";
+bool CheckRow(short i, short j, short num) {
+	for (int k = 0; k < 9; k++) {
+		if (sudoku[i][k] == num) {
+			return false;
+		}
 	}
-}
-
-bool isSafe(vector<vector<int>>& s, int row, int col, int num)
-{
-	for (int i = 0; i < SUDOKU_SIZE; i++)
-		if (s[row][i] == num)
-			return false;
-
-	for (int i = 0; i < SUDOKU_SIZE; i++)
-		if (s[i][col] == num)
-			return false;
-
-	int smallRow = row - row % SMALL_SUDOKU_SIZE;
-	int smallCol = col - col % SMALL_SUDOKU_SIZE;
-
-	for (int i = 0; i < SMALL_SUDOKU_SIZE; i++)
-		for (int j = 0; j < SMALL_SUDOKU_SIZE; j++)
-			if (s[i + smallRow][j + smallCol] == num)
-				return false;
-	
 	return true;
 }
 
-bool solveSudoku(vector<vector<int>>& s, int row, int col)
-{
-	if (row == SUDOKU_SIZE - 1 && col == SUDOKU_SIZE)
-		return true;
-	
-	if (col == SUDOKU_SIZE)
-	{
-		row++;
-		col = 0;
-	}
-
-	if (s[row][col] > 0)
-		return solveSudoku(s, row, col + 1);
-
-	for (int num = 1; num <= SUDOKU_SIZE; num++)
-	{
-		if (isSafe(s, row, col, num))
-		{
-			s[row][col] = num;
-
-			if (solveSudoku(s, row, col + 1))
-				return true;
+bool CheckCol(short i, short j, short num) {
+	for (int k = 0; k < 9; k++) {
+		if (sudoku[k][j] == num) {
+			return false;
 		}
+	}
+	return true;
+}
 
-		s[row][col] = 0;
+bool CheckSquare(short i, short j, short num) {
+		short startRow = i - i % 3;
+	short startCol = j - j % 3;
+	for (int k = startRow; k < startRow + 3; k++) {
+		for (int l = startCol; l < startCol + 3; l++) {
+			if (sudoku[k][l] == num) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void SolveSudoku(int idx) {
+	if (idx == targets.size()) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				cout << sudoku[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		exit(0);
 	}
 
-	return false;
+	for (int i = idx; i < targets.size(); i++) {
+		short row = targets[i].first;
+		short col = targets[i].second;
+		for (short j = 1; j <= 9; j++) {
+			if (CheckRow(row, col, j) && CheckCol(row, col, j) && CheckSquare(row, col, j)) {
+				sudoku[row][col] = j;
+				SolveSudoku(idx + 1);
+				sudoku[row][col] = 0;
+			}
+		}
+		return;
+	}
 }
 
 int main()
 {
-	fastio;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-	vector<vector<int>> sudoku(SUDOKU_SIZE, vector<int>(SUDOKU_SIZE, 0));
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> sudoku[i][j];
+			if (sudoku[i][j] == 0) {
+				targets.push_back({ i, j });
+			}
+		}
+	}
 
-	for (auto& i : sudoku)
-		for (auto& j : i)
-			cin >> j;
-
-	if (solveSudoku(sudoku, 0, 0))
-		print(sudoku);
-	else
-		cout << "No solution exists";
+	SolveSudoku(0);
 
 	return 0;
 }
