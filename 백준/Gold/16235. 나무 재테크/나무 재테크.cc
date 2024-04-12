10ffb94e161e984201dd9869d9bea750a73e602b
Hanyoung Lee <kakaoleaves@naver.com>
2024-04-12 23:10:23 +0900

    [Gold III] Title: 나무 재테크, Time: 244 ms, Memory: 2428 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -16,7 +16,8 @@ map<pair<int, int>, vector<int>> tree; // 나무의 위치와 나이
 map<pair<int, int>, int> dead_tree; // 죽은 나무의 위치와 영양분 정보
 map<pair<int, int>, int> breed_tree; // 번식하는 나무의 위치와 개수
 
-int main() {
+void Init()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -35,90 +36,112 @@ int main() {
     for (int i = 0; i < m; i++)
     {
         int x, y, z;
-		cin >> x >> y >> z;
+        cin >> x >> y >> z;
         tree[{x, y}].push_back(z);
     }
+}
+
+void Spring()
+{
+    for (auto& t : tree)
+    {
+        auto [coords, ages] = t;
+        auto [r, c] = coords;
+        sort(ages.begin(), ages.end());
+
+        vector<int> new_ages;
+        for (const auto& age : ages)
+        {
+            if (soils[r][c] >= age)
+            {
+                soils[r][c] -= age;
+                new_ages.push_back(age + 1);
+
+                if ((age + 1) % 5 == 0)
+                {
+                    breed_tree[{r, c}]++;
+                }
+
+            }
+            else
+            {
+                dead_tree[{r, c}] += age / 2;
+            }
+        }
+        tree[coords] = new_ages;
+    }
+}
+
+void Summer()
+{
+    for (const auto& d : dead_tree)
+    {
+        auto [coords, nutrient] = d;
+        auto [r, c] = coords;
+        soils[r][c] += nutrient;
+    }
+    dead_tree.clear();
+}
+
+void Fall()
+{
+    for (const auto& b : breed_tree)
+    {
+        const auto& [coords, cnt] = b;
+        const auto& [r, c] = coords;
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue; // 자기 자신은 제외
+                int nr = r + i;
+                int nc = c + j;
+                if (nr < 1 || nr > n || nc < 1 || nc > n) continue; // 범위를 벗어나면 제외
+                tree[{nr, nc}].insert(tree[{nr, nc}].begin(), cnt, 1);
+            }
+        }
+    }
+    breed_tree.clear();
+}
+
+void Winter()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            soils[i][j] += a[i][j];
+        }
+    }
+}
+
+void Conclude()
+{
+    int ans = 0;
+
+    for (auto it = tree.begin(); it != tree.end(); it++)
+    {
+        ans += it->second.size();
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    Init();
 
     while (k--)
     {
         if (tree.empty()) break;
 
-        // 봄
-        for (auto& t : tree)
-        {
-            auto [coords, ages] = t;
-            auto [r, c] = coords;
-            sort(ages.begin(), ages.end());
-
-            vector<int> new_ages;
-            for (const auto& age : ages)
-            {
-                if (soils[r][c] >= age)
-                {
-                    soils[r][c] -= age;
-                    new_ages.push_back(age + 1);
-
-                    if ((age + 1) % 5 == 0)
-                    {
-						breed_tree[{r, c}]++;
-					}
-
-                }
-                else
-                {
-                    dead_tree[{r, c}] += age / 2;
-                }
-            }
-            tree[coords] = new_ages;
-        }
-
-        // 여름
-        for (const auto& d : dead_tree)
-        {
-			auto [coords, nutrient] = d;
-			auto [r, c] = coords;
-			soils[r][c] += nutrient;
-		}
-        dead_tree.clear();
-
-        // 가을
-        for (const auto& b : breed_tree)
-        {
-            const auto& [coords, cnt] = b;
-            const auto& [r, c] = coords;
-
-            for (int i = -1; i <= 1; i++)
-            {
-                for (int j = -1; j <= 1; j++)
-                {
-                    if (i == 0 && j == 0) continue; // 자기 자신은 제외
-                    int nr = r + i;
-                    int nc = c + j;
-                    if (nr < 1 || nr > n || nc < 1 || nc > n) continue; // 범위를 벗어나면 제외
-                    tree[{nr, nc}].insert(tree[{nr, nc}].begin(), cnt, 1);
-                }
-            }
-        }
-        breed_tree.clear();
-
-        // 겨울
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-				soils[i][j] += a[i][j];
-			}
-		}
+        Spring();
+        Summer();
+        Fall();
+        Winter();       
     }
 
-    int ans = 0;
-
-    for (auto it = tree.begin(); it != tree.end(); it++)
-    {
-		ans += it->second.size();
-	}
-
-    cout << ans << '\n';
+    Conclude();
 
     return 0;
 }
