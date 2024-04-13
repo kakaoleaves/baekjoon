2383f7c46cf056728cc8e951a678cbcc9b645ab1
Hanyoung Lee <kakaoleaves@naver.com>
2024-04-13 23:42:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 3356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,58 +1,89 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
+
 using namespace std;
 
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	int v; // 1 <= v <= 10'000
-	int e; // 1 <= e <= 100'000
-	int ans = 0;
-
-	cin >> v >> e;
-
-	vector<int> vertices(v + 1);
-	vector<vector<pair<int, int>>> graph(v + 1);
-
-	for (int i = 0; i < e; i++)
-	{
-		int a, b, c; // a, b <= v, c <= 1'000
-		cin >> a >> b >> c;
-		graph[a].push_back({ b, c });
-		graph[b].push_back({ a, c });
-	}
-
-	vector<bool> visited(v + 1, false);
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push({ 0, 1 });
-
-	while (!pq.empty()) {
-		int cost = pq.top().first;
-		int vertex = pq.top().second;
-
-		pq.pop();
-
-		if (visited[vertex]) continue;
-
-		visited[vertex] = true;
-
-		ans += cost;
-
-		for (auto& edge : graph[vertex]) {
-			int nextVertex = edge.first;
-			int nextCost = edge.second;
-
-			if (!visited[nextVertex]) {
-				pq.push({ nextCost, nextVertex });
-			}
+class DisjointSet {
+private:
+	vector<int> parent;
+	vector<int> rank;
+public:
+	DisjointSet(int n) : parent(n), rank(n, 1) {
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
 		}
 	}
 
-	cout << ans << '\n';
+	int findSet(int a)
+	{
+		if (parent[a] == a) return a;
+		return parent[a] = findSet(parent[a]);
+	}
 
+	void unionSet(int a, int b)
+	{
+		int rootA = findSet(a);
+		int rootB = findSet(b);
+
+		if (rootA == rootB) return;
+		
+		if (rank[rootA] > rank[rootB]) {
+			parent[rootB] = rootA;
+		}
+		else {
+			parent[rootA] = rootB;
+			if (rank[rootA] == rank[rootB]) {
+				rank[rootB]++;
+			}
+		}
+	}
+};
+
+int v;
+int e;
+int ans;
+vector<pair<int, pair<int, int>>> edges(100001);
+
+void Init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> v >> e;
+	
+	edges.resize(e);
+	for (int i = 0; i < e; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		edges[i] = { c, {a, b} };
+	}	
+
+	// sort by cost
+	sort(edges.begin(), edges.end());
+}
+
+void Kruskal()
+{
+	DisjointSet ds(v + 1);
+
+	for (int i = 0; i < e; i++) {
+		int a = edges[i].second.first;
+		int b = edges[i].second.second;
+		int c = edges[i].first;
+
+		if (ds.findSet(a) == ds.findSet(b)) continue;
+
+		ds.unionSet(a, b);
+		ans += c;
+	}
+}
+
+int main()
+{
+	Init();
+	Kruskal();
+	cout << ans << '\n';
 	return 0;
 }
